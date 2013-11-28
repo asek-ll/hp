29c29cd12ec31f9443530979c6858fed5a6d483a
OpenShift guest <sopplet@gmail.com>
2013-11-28 09:45:11 -0500

    dump commit

--- config.js
+++ config.js
@@ -2,11 +2,7 @@ var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 var ip = process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0";
 var host = process.env.OPENSHIFT_APP_DNS || "localhost:"+port;
 
-var mongodb = {
-  "user": "admin",
-  "password": "ezwElyNDri-T",
-  "database": "hp"
-};
+var mongodb = {};
 
 module.exports = [
   { packagePath: "./plugins/express",
